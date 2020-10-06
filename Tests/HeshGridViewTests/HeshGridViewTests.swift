e6e7d438d81b79fbf8edbd0622d998d8009a7b8a
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-06 17:27:41 +0530

    tes

--- Tests/HeshGridViewTests/HeshGridViewTests.swift
+++ Tests/HeshGridViewTests/HeshGridViewTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class HeshGridViewTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HeshGridView().text, "Hello, World!")
+        
     }
 
     static var allTests = [
